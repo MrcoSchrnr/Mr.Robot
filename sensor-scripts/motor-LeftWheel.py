b49035ad1e898116ca897d0a2a4aa398e892878a
MrRobotObjectDetection@gmail.com <MrRobotObjectDetection@gmail.com>
2020-03-05 16:23:57 +0100

    updated motor controll | works now with functions

--- sensor-scripts/motor-LeftWheel.py
+++ sensor-scripts/motor-LeftWheel.py
@@ -8,58 +8,13 @@ GPIO_PWM = 13 # PWM pin
 GPIO_IN1 = 27 # motor control pin
 GPIO_IN2 = 22 # motor control pin
 
-"""
-not necessary in new code
-
-RUNNING_TIME = 5 # seconds
-PWM_FREQUENCY = 50000 # Hz
-PWM_DUTY_CYCLE = 10 # percent
-"""
-
 pi = pigpio.pi()
-
-# For PWM on non-hardware-GPIO pins (here: 21)
-#pi.set_mode(21, pigpio.OUTPUT)
-#pi.set_PWM_frequency(21, PWM_FREQUENCY)
-#pi.set_PWM_range(21, 100)
-#pi.set_PWM_dutycycle(PWM_DUTY_CYCLE)
 
 # Motor control pins
 # 0, 1 forward (whatever that means in your case)
 # 1, 0 reverse
 # 1, 1 brake
 # 0, 0 stop slowly
-
-"""
-Old code of the script of Mr. Funk
-
-pi.set_mode(GPIO_IN1, pigpio.OUTPUT)
-pi.set_mode(GPIO_IN2, pigpio.OUTPUT)
-pi.write(GPIO_IN1, 0)
-pi.write(GPIO_IN2, 1)
-
-try:
-    duty = PWM_DUTY_CYCLE * 10000 # Max: 1M
-    pi.hardware_PWM (GPIO_PWM, PWM_FREQUENCY, duty)
-    print('Hardware PWM on GPIO ' + str(GPIO_PWM) + ' enabled')
-    print('Frequency: ' + str(pi.get_PWM_frequency(GPIO_PWM)) + ' Hz')
-    print('Dutycycle: ' + \
-          str(pi.get_PWM_dutycycle(GPIO_PWM) / 10000) + ' percent')
-    print('Running for ' + str(RUNNING_TIME) + ' seconds')
-    running = True
-except:
-    print('Hardware PWM not available on GPIO ' + str(GPIO_PWM))
-    pi.stop()
-
-if running:
-    time.sleep(RUNNING_TIME)
-    print('Shutting down')
-    # Disable PWM
-    pi.write(GPIO_PWM, 0)
-    # Free resources
-    pi.stop()
-"""
-
 
 def drive(ahead = True, frequency = 50000, duty_cycle = 10):
 
@@ -79,12 +34,12 @@ def drive(ahead = True, frequency = 50000, duty_cycle = 10):
         pi.write(GPIO_IN2, 0)
 
     try:
-        duty = duty_cycle * 10000 # Max: 1M
-        pi.hardware_PWM (GPIO_PWM, PWM_FREQUENCY, PWM_DUTY_CYCLE)
+        duty = PWM_DUTY_CYCLE * 10000 # Max: 1M
+        pi.hardware_PWM (GPIO_PWM, PWM_FREQUENCY, duty)
         print('Hardware PWM on GPIO ' + str(GPIO_PWM) + ' enabled')
         print('Frequency: ' + str(pi.get_PWM_frequency(GPIO_PWM)) + ' Hz')
         print('Dutycycle: ' + \
-                str(pi.get_PWM_dutycycle(GPIO_PWM) / 10000) + ' percent')
+              str(pi.get_PWM_dutycycle(GPIO_PWM) / 10000) + ' percent')
 
     except: 
         print('Hardware PWM not available on GPIO ' + str(GPIO_PWM))
@@ -96,8 +51,17 @@ def stop():
     pi.write(GPIO_IN1, 1)
     pi.write(GPIO_IN2, 1)
 
+def shutDown():
     #disable PWM
     pi.write(GPIO_PWM, 0)
 
     # Free resources
     pi.stop()
+
+def test():
+    drive(True, frequency=50000, duty_cycle=50)
+    time.sleep(5)
+    stop()
+    shutDown()
+
+test()
