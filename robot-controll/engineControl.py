7b43173d6bc05fd0780890432387acba81857896
MrRobotObjectDetection@gmail.com <MrRobotObjectDetection@gmail.com>
2020-07-15 01:01:24 +0200

    synchronizing threads

--- robot-controll/engineControl.py
+++ robot-controll/engineControl.py
@@ -155,13 +155,18 @@ class Engines:
 
 
     def stopAllEngines(self):
-        engineBack = Thread(target=self.stopEngineBack())
-        engineLeft = Thread(target=self.stopEngineLeft())
-        engineRight = Thread(target=self.stopEngineRight())
+        engineBack = Thread(target=self.stopEngineBack)
+        engineLeft = Thread(target=self.stopEngineLeft)
+        engineRight = Thread(target=self.stopEngineRight)
 
         engineBack.start()
         engineLeft.start()
         engineRight.start()
+
+        engineBack.join()
+        engineLeft.join()
+        engineRight.join()
+
 
     """ 
         in this area there are some additional funtions for shutting down the engines and getting the current data of the engines
